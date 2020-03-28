99b02cf42918b13e93837ab0307d49e2f15eedbf
Darshan-BA <ba8496@gmail.com>
2020-03-28 12:44:56 +0530

    Entity class[Contact.class] created

---
+++ app/src/main/java/com/ba/contacts/Contact.java
@@ -0,0 +1,82 @@
+package com.ba.contacts;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName ="contacts")
+public class Contact {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "first_name")
+    private String firstName;
+
+    @ColumnInfo(name = "last_name")
+    private String lastName;
+
+    @ColumnInfo(name = "primary_number")
+    private int primaryPhoneNumber;
+
+    @ColumnInfo(name="secondary_number")
+    private int secondaryPhoneNumber;
+
+    @ColumnInfo(name = "email_id")
+    private  String emailId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getPrimaryPhoneNumber() {
+        return primaryPhoneNumber;
+    }
+
+    public int getSecondaryPhoneNumber() {
+        return secondaryPhoneNumber;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPrimaryPhoneNumber(int primaryPhoneNumber) {
+        this.primaryPhoneNumber = primaryPhoneNumber;
+    }
+
+    public void setSecondaryPhoneNumber(int secondaryPhoneNumber) {
+        this.secondaryPhoneNumber = secondaryPhoneNumber;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public Contact(String firstName, String lastName, int primaryPhoneNumber, int secondaryPhoneNumber, String emailId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.primaryPhoneNumber = primaryPhoneNumber;
+        this.secondaryPhoneNumber = secondaryPhoneNumber;
+        this.emailId = emailId;
+    }
+}
