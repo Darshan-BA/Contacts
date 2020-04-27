42df6cf2f3490496b9fd4fe421ea4ff79010e4f0
Darshan-BA <ba8496@gmail.com>
2020-04-27 21:05:18 +0530

    settype changed in import Contacts

--- app/src/main/java/com/ba/contacts/MainActivity.java
+++ app/src/main/java/com/ba/contacts/MainActivity.java
@@ -156,8 +156,6 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void setContextualActionMode() {
-                //adapter.setSetMultiDelete(true);
-                //adapter.notifyDataSetChanged();
                 adapter.setMultiDelete = true;
                 adapter.notifyDataSetChanged();
                 toolbar.startActionMode(actionModeCallback);
@@ -350,7 +348,7 @@ public class MainActivity extends AppCompatActivity {
         }
         if (item.getItemId() == R.id.import_menu_item) {
             Log.d("BA", "On Import Clicked");
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ) {
                 if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
                     Toast.makeText(MainActivity.this, "Need READ permission to Import Contacts ", Toast.LENGTH_LONG).show();
                     ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_EXTERNAL_READ);
@@ -360,9 +358,9 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("text/json");
+                intent.setType("*/*");
                 startActivityForResult(intent, PICK_JSON_FILE);
-            }
+           }
         }
 
         if (item.getItemId() == R.id.export_menu_item) {
@@ -409,7 +407,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent,PICK_JSON_FILE);
             }
             else {
-                Toast.makeText(MainActivity.this, "Grant Read Permission To Export Contacts", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Grant Read Permission To Import Contacts", Toast.LENGTH_LONG).show();
             }
         }
         if (requestCode == PERMISSION_EXTERNAL_WRITE) {
